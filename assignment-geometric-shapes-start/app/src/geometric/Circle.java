25208ad9bd3a1eb43fda0e29f704baf9908faa36
ItsSven64 <svendewit1707@gmail.com>
2024-02-16 14:53:48 +0100

    Finished working on Circle class

--- assignment-geometric-shapes-start/app/src/geometric/Circle.java
+++ assignment-geometric-shapes-start/app/src/geometric/Circle.java
@@ -1,5 +1,35 @@
 package geometric;
 
-public class Circle {
+public class Circle implements Geometric{
+    private double[] Centre = new double[2];
+    private double Radius;
+
+    public Circle(double[] origin, double radius){
+        Centre = origin;
+        Radius = radius;
+    }
     
+        //To make rectangle, get the origin
+        //Then add, or subtract the radius to the x or y coords depending on the desired side
+    public double get_left_rect(){
+        return this.Centre[0]-this.Radius;
+    }
+    public double get_right_rect(){
+        return this.Centre[0]+this.Radius;
+    }
+    public double get_bottom_rect(){
+        return this.Centre[1]-this.Radius;
+    }
+    public double get_top_rect(){
+        return this.Centre[1]+this.Radius;
+    }
+    public double get_area(){
+        //Pi = 3.1415
+        //Area = Pi*r*r (Pi*r^2)
+        return 3.1415*this.Radius*this.Radius;
+    }
+    public void move_object(int dx, int dy){
+        this.Centre[0] += dx;
+        this.Centre[1] += dy;
+    }
 }
--- assignment-geometric-shapes-start/app/src/geometric/Geometric.java
+++ assignment-geometric-shapes-start/app/src/geometric/Geometric.java
@@ -1,5 +1,10 @@
 package geometric;
 
 interface Geometric{
-    public double get_
+    public double get_left_rect();
+    public double get_right_rect();
+    public double get_bottom_rect();
+    public double get_top_rect();
+    public double get_area();
+    public void   move_object(int dx, int dy);
 }
