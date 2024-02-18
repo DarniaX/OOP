cea4ac8be5c4657291cf2b4939acf07563bdbc0f
DarniaX <96333108+DarniaX@users.noreply.github.com>
2024-02-18 23:04:53 +0100

    int -> double

--- assignment-geometric-shapes-start/app/src/geometric/Geometric.java
+++ assignment-geometric-shapes-start/app/src/geometric/Geometric.java
@@ -6,7 +6,7 @@ interface Geometric{
     public double get_bottom_rect();
     public double get_top_rect();
     public double get_area();
-    public void   move_object(int dx, int dy);
+    public void   move_object(double dx, double dy);
 }
 
 interface GeometricPredicate{
